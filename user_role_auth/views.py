fbdac4fd37c68c25ffd22dd0e1e07202686ee004
Rahul Rajeev <rahulrajeev836@gmail.com>
2025-05-15 00:35:45 +0530

    working on basic setup and login signup

---
+++ user_role_auth/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        # Handle login logic here
+        pass
+    return render(request, "login.html")
+
+
+def register_view(request):
+    return render(request, "signup.html")
